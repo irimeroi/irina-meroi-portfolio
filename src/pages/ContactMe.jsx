f3c35ca61b4d31454907ff65249d8159f53b2f3f
Irina Meroi <irimeroi7@gmail.com>
2024-02-19 18:09:23 -0300

    added validation fields to form

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -6,6 +6,7 @@ function ContactInfo() {
     const [info, setInfo] = useState({ name: '', email: '', message: ''});
     const { name, email, message } = info;
     const [errorMessage, setErrorMessage] = useState('');
+    const [validated, setValidated] = useState(false);
     
     function handleValidation (event) {
         event.preventDefault();
@@ -16,37 +17,40 @@ function ContactInfo() {
         })
         
         if (!validateEmail(event.target.value)) {
-            setErrorMessage('Please add a valid email address');
+            setErrorMessage('Please enter a valid email address');
         } else {
             setErrorMessage('');
         }
     }
 
     function handleSubmit(event) {
-        event.preventDefault();
-        if (info.name && info.email && info.message) {
-            window.location.reload();
-        } else {
-            alert('Please fill out all fields')
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
         }
+        setValidated(true);
       }
 
     return (
-        <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit} >
             <Form.Group className="pt-2" controlId="exampleForm.ControlInput1">
                 <Form.Label className='pt-4'>Name</Form.Label>
-                <Form.Control type="text" placeholder="Your name here" name="name" onChange={handleValidation} />
+                <Form.Control type="text" placeholder="Your name here" name="name" required />
+                <Form.Control.Feedback type='invalid' className='text-white shadow-sm p-2 mb-1 rounded'>Please fill out this field!</Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="pt-2" controlId="exampleForm.ControlInput1">
                 <Form.Label className='pt-4'>Email address</Form.Label>
-                <Form.Control type="email" name="email" onChange={handleValidation} placeholder="name@example.com" />
+                <Form.Control type="email" name="email" placeholder="name@example.com" onChange={handleValidation} required/>
                 {errorMessage && <p className="error-message">{errorMessage}</p>}
+                <Form.Control.Feedback type='invalid' className='text-white shadow-sm p-2 mb-1 rounded'>Please fill out this field!</Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="pt-2" controlId="exampleForm.ControlTextarea1">
                 <Form.Label className='pt-4'>Message</Form.Label>
-                <Form.Control className='text-area' as="textarea" name="message" onChange={handleValidation} rows={3} />
+                <Form.Control className='text-area' as="textarea" name="message" rows={3} required/>
+                <Form.Control.Feedback type='invalid' className='text-white shadow-sm p-2 mb-1 rounded'>Please fill out this field!</Form.Control.Feedback>
             </Form.Group>
-            <button id='submit-form' onClick={handleSubmit} >Submit</button>
+            <button id='submit-form' type='submit'>Submit</button>
         </Form>
     )
 }
