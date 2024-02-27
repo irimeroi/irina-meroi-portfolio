d07736630822e1b4fb7b1cbc443cf6770c3d693b
Irina Meroi <irimeroi7@gmail.com>
2024-02-27 01:27:06 -0300

    changed twitter to email

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <footer className="footer fixed-bottom mt-5 d-flex justify-content-center">
             <a href="https://github.com/irimeroi"><img src="assets/logos/github-mark-white.png" alt="Github" className="icon" /></a>
             <a href="https://www.linkedin.com/in/irina-meroi-385bbb245/"><img src="assets/logos/Linkedin.png" alt="LinkedIn" className="icon" /></a>
-            <a href="https://twitter.com/irimeroi7"><img src="assets/logos/Twitter.png" alt="Twitter" className="icon" /></a>
+            <a href="mailto:irimeroi7@gmail.com"><img src="assets/logos/Mail.png" alt="Email" className="icon" /></a>
         </footer>
     )
 }
