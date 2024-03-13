7ee74caf0c21736c7c4d4329388ea59ac5efa18d
Irina Meroi <irimeroi7@gmail.com>
2024-03-12 23:18:56 -0700

    max-width screen links changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import './App.css';
 import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
+import Navbar from './pages/home/Navbar';
 import Home from './pages/home/homescreen';
-import Header from './pages/home/Header'
 import Footer from './pages/home/Footer';
 
 function App() {
   
   return (
     <>
-      <Home />
-      {/* <Header />
-      <Footer /> */}
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </>
   )
 }
--- src/pages/home/Navbar.jsx
+++ src/pages/home/Navbar.jsx
@@ -1,18 +1,42 @@
 // import { useState, useEffect } from "react";
 // import { Link } from "react-scroll";
 
-function Navbar(props) {
-    const { setCurrentPage } = props;
+// function Navbar(props) {
+//     const { setCurrentPage } = props;
+//     return (
+//         <nav className='d-inline'>
+//             <div className='d-flex justify-content-around flex-wrap'>
+//                 <button id='main-btn' onClick={() => setCurrentPage('herosection')}>Irina Meroi</button>
+//                 <button className="nav-btn border" onClick={() => setCurrentPage('portfolio')}>Portfolio</button>
+//                 <button className="nav-btn border" onClick={() => setCurrentPage('contactme')}>Contact Me</button>
+//                 <button className="nav-btn border" onClick={() => setCurrentPage('resume')}>Resume</button>
+//             </div>
+//         </nav>
+//     );
+// }
+
+// export default Navbar;
+
+import React, {useState} from 'react';
+
+function Navbar() {
+    const [display, setDisplay] = useState(false);
+
     return (
-        <nav className='d-inline'>
-            <div className='d-flex justify-content-around flex-wrap'>
-                <button id='main-btn' onClick={() => setCurrentPage('herosection')}>Irina Meroi</button>
-                <button className="nav-btn border" onClick={() => setCurrentPage('portfolio')}>Portfolio</button>
-                <button className="nav-btn border" onClick={() => setCurrentPage('contactme')}>Contact Me</button>
-                <button className="nav-btn border" onClick={() => setCurrentPage('resume')}>Resume</button>
-            </div>
-        </nav>
-    );
+        <div>
+            <nav className="d-flex justify-content-around align-items-center">
+                <div id="main-btn" className="name">Irina Meroi</div>
+                <ul className="nav-links d-flex justify-content-around" style={{ transform: display ? "translateX(0px)" : ""}}>
+                    <li><a className="nav-btns">About</a></li>
+                    <li><a className="nav-btns">Portfolio</a></li>
+                    <li><a className="nav-btns">Contact</a></li>
+                    <li><a className="nav-btns">Resume</a></li>
+                </ul>
+                <i class="fa-solid fa-bars bars-icon" onClick={() => setDisplay(!display)} ></i>
+            </nav>
+        </div>
+
+    )
 }
 
 export default Navbar;
